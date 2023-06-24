dbd135e37007972040980527366f6a65c5a652e0
bcaballo04 <bcaballo@umich.edu>
2023-06-24 16:39:26 -0400

    Added hover eventListener

--- etch.js
+++ etch.js
@@ -3,8 +3,21 @@ const container = document.querySelector("div")
 for(let x = 1; x <= (16*16); x++){
     const box = document.createElement("div");
     box.classList.add("box")
-    box.textContent = x;
     container.appendChild(box);
 }
+
+const boxes = document.querySelectorAll("div.box")
+boxes.forEach(function (box){
+
+box.addEventListener('mouseover', function (e) {
+    box.classList.add("hover");
+});
+
+box.addEventListener('mouseout', function (e) {
+    box.classList.remove("hover");
+});
+
+});
+
 
 
